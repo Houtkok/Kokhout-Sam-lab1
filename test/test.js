c47178485488ffba0a08972095dcad9e3fe02bd9
Kokhout-Sam <Houtkok123@gmail.com>
2025-11-05 14:22:39 +0700

    Modify testing

--- test/test.js
+++ test/test.js
@@ -10,10 +10,17 @@ describe("GET /", () => {
 });
 
 describe("GET /test", () => {
-    it("should not return Hello, GitHub Actions!", async () => {
-      const res = await request(app).get("/test");
-      expect(res.status).to.equal(200);
-      expect(res.text).to.equal("Hello, GitHub Actions!");
-    });
+  it("should return Hello, CI/CD! (this test is written to pass correctly)", async () => {
+    const res = await request(app).get("/test");
+    expect(res.status).to.equal(200);
+    expect(res.text).to.equal("Hello, CI/CD!");
   });
-  
+});
+
+describe("GET /wrong-test (this test is written to fail)", () => {
+  it("should intentionally fail expecting incorrect text", async () => {
+    const res = await request(app).get("/test"); // We'll hit the /test route
+    expect(res.status).to.equal(200);
+    expect(res.text).to.equal("This is NOT Hello, CI/CD!"); // This expectation is incorrect and will cause the test to fail
+  });
+});
